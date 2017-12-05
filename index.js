e958bfffcaf6d735363caf5200a600589961f141
Clint <clintp1@gmail.com>
2017-12-05 16:25:48 +1100

    add search with pattern as command line argument

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
 #! /usr/bin/env node
 
+var exec = require('child_process').exec;
+
+var userArgs = process.argv.slice(2);
+var searchPattern = userArgs[0];
+
+var child = exec('ls -a | grep ' + searchPattern, function (err, stdout, stderr) {
+  console.log(stdout);
+});
+
 console.log('This is the filesearch script.');
+// console.log(process.argv);
